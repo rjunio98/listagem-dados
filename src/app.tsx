66e01e6bf17bdbf4a48589d26bc1e0aed5921a03
rjunio98 <robledorjunior1@gmail.com>
2024-02-26 12:21:00 -0300

    Adiciona componentes Header e Tabs, e estilos para botão e input

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,38 @@
+import { Plus, Search, FileDown } from "lucide-react";
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
+import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
+
 export function App() {
   return (
-    <div>
-      <h1>fdfdfd</h1>
+    <div className="py-10 space-y-8">
+      <div>
+        <Header />
+        <Tabs />
+      </div>
+
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Button variant="primary">
+            <Plus className="size-3" />
+            Create new
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <Input variant="filter">
+            <Search className="size-3" />
+            <Control placeholder="Search tags..." />
+          </Input>
+
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
+        </div>
+      </main>
     </div>
   );
 }
